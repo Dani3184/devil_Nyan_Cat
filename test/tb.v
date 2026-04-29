3f8a4d4158afbbbd48bc00c0e915944a95c2c88d
Dani_318 <danielrgm222@gmail.com>
2026-04-28 22:40:07 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,16 +1,17 @@
 //Author: Daniel Roberto Garcia Miranda
 //Institution: Universidad Mayor de San Andres, Physics Career, Cosmic Ray Group
-
 `default_nettype none
 `timescale 1ns / 1ps
 
-// Passive testbench for TinyTapeout + cocotb
+/* Simple testbench to instantiate the user project 
+   and dump waveform data for GTKWave.
+*/
 
 module tb ();
-
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
+    #1;
   end
 
   reg clk;
@@ -18,24 +19,22 @@ module tb ();
   reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
-
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  tt_um_dual_nyancat user_project (
+  // Instantiate the project with the new module name
+  tt_um_devil_nyancat user_project (
 `ifdef GL_TEST
-    .VPWR(1'b1),
-    .VGND(1'b0),
+      .VPWR(1'b1), .VGND(1'b0),
 `endif
-    .ui_in(ui_in),
-    .uo_out(uo_out),
-    .uio_in(uio_in),
-    .uio_out(uio_out),
-    .uio_oe(uio_oe),
-    .ena(ena),
-    .clk(clk),
-    .rst_n(rst_n)
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
   );
-
 endmodule
