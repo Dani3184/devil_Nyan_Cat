ca0e9b220c9dfedfe98e628582affde6824c9c05
Dani_318 <danielrgm222@gmail.com>
2026-04-30 23:42:29 -0400

    TBV

--- test/tb.v
+++ test/tb.v
@@ -15,6 +15,7 @@ module tb ();
     #1;
   end
 
+  // Signals for the project
   reg clk;
   reg rst_n;
   reg ena;
@@ -24,20 +25,24 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+  // --- POWER AND GROUND WIRES ---
+  wire vcc = 1'b1;
+  wire gnd = 1'b0;
+
   // Instantiating the Devil Nyan Cat module
   tt_um_devil_nyancat user_project (
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(vcc),      // Connected to wire vcc
+      .VGND(gnd),      // Connected to wire gnd
 `endif
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
+      .ui_in   (ui_in),
+      .uo_out  (uo_out),
+      .uio_in  (uio_in),
+      .uio_out (uio_out),
+      .uio_oe  (uio_oe),
+      .ena     (ena),
+      .clk     (clk),
+      .rst_n   (rst_n)
   );
 
 endmodule
